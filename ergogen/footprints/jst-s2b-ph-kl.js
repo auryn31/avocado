907f9a4d907d0a0cf203b4a100155c439442367c
Auryn Engel <auryn.engel@oviva.com>
2024-11-03 13:23:17 +0100

    update pinpout

--- ergogen/footprints/jst-s2b-ph-kl.js
+++ ergogen/footprints/jst-s2b-ph-kl.js
@@ -1,9 +1,9 @@
 module.exports = {
-    params: {
-        pos: { type: 'net', value: 'pos' },
-        neg: { type: 'net', value: 'neg' },
-    },
-    body: p => `
+  params: {
+    pos: { type: "net", value: "pos" },
+    neg: { type: "net", value: "neg" },
+  },
+  body: (p) => `
         (footprint "Connector_JST:JST_PH_S2B-PH-K_1x02_P2.00mm_Horizontal"
             ${p.at}
             (descr "JST PH series connector, S2B-PH-K (http://www.jst-mfg.com/product/pdf/eng/ePH.pdf), generated with kicad-footprint-generator")
@@ -83,7 +83,19 @@ module.exports = {
             (fp_line (start 3.95 6.25) (end 3.95 -1.35) (stroke (width 0.1) (type solid)) (layer "B.Fab"))
 
             (pad "1" thru_hole roundrect (at 0 0 ${p.rot}) (size 1.2 1.75) (drill 0.75) (layers "*.Cu" "*.Mask") (roundrect_rratio 0.208333) ${p.pos.str})
+            (fp_text user "+" (at 0 -1.25 ${p.rot + 90}) (layer F.SilkS)
+              (effects (font (size 0.5 0.5) (thickness 0.1)))
+            )
+            (fp_text user "+" (at 0 -1.25 ${p.rot + 90}) (layer B.SilkS)
+              (effects (font (size 0.5 0.5) (thickness 0.1)))
+            )
             (pad "2" thru_hole oval (at 2 0 ${p.rot}) (size 1.2 1.75) (drill 0.75) (layers "*.Cu" "*.Mask") ${p.neg.str})
+            (fp_text user "-" (at 2 -1.25 ${p.rot + 90}) (layer F.SilkS)
+              (effects (font (size 0.5 0.5) (thickness 0.1)))
+            )
+            (fp_text user "-" (at 2 -1.25 ${p.rot + 90}) (layer B.SilkS)
+              (effects (font (size 0.5 0.5) (thickness 0.1)))
+            )
         )
-    `
-}
+    `,
+};
